53ddd6625d778b9a2724a61ea7bada370fa5d493
Pierre Martin <pierre.martin49@gmail.com>
2013-11-14 14:49:23 +0100

    Ajout fichiers de base

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pierre MARTIN - Développeur Web</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Page description">
+		<meta name="author" content="Pierre MARTIN" >
+		<meta charset="utf-8">
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+		<link href="css/main.css" rel="stylesheet">
+
+		<!--[if IE 7]>
+			<link href="css/font-awesome-ie7.css" rel="stylesheet">
+		<![endif]-->
+
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!-- Import JQuery -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		
+	</head>
+
+	<body>
+
+		<header id="header">
+			
+		</header>
+
+		<section>
+			<div id="wrapper">
+			<form id="login" class="front box">
+			  <div class="default"><i class="icon-briefcase"></i><h1>Press login</h1></div>
+			<input type="text" placeholder="username" />
+			<input type="password" placeholder="password" />
+			<button class="login"><i class="icon-ok"></i></button>
+			</form>
+
+			<div class="back box">
+			<img src="http://i.imgur.com/sdDkYt1.png"/>
+			<ul>
+			  <li><i class="icon-file"></i> New <span>32</span></li>
+			  <li><i class="icon-flag"></i> Priority <span>12</span></li>
+			  <li><i class="icon-user"></i> Assigned <span>5/17</span></li>
+			  <li><i class="icon-trash"></i> Deleted <span>14</span></li>  
+			</ul>
+			<button class="logout"><i class="icon-off"></i></button>
+			</div>
+			</div>
+		</section>
+
+		<footer id="footer">
+			
+		</footer>
+
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+	</body>
+
+</html>
